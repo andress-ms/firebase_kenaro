cc85686f501ef63cae7a542c0c74d092e0d5733a
andress-ms <amartinezs@uamv.edu.ni>
2025-06-05 18:31:42 -0600

    changed dependencies

--- kenaro_firebase/app/src/main/java/com/amms/kenaro/viemodel/ProductViewModel.kt
+++ kenaro_firebase/app/src/main/java/com/amms/kenaro/viemodel/ProductViewModel.kt
@@ -8,54 +8,25 @@ import com.amms.kenaro.dataclass.Product
 import kotlinx.coroutines.launch
 
 class ProductViewModel : ViewModel() {
-    private val _products = mutableStateListOf<Product>()
-    val products: List<Product> = _products
 
-    val isLoading = mutableStateOf(false)
-    val errorMessage = mutableStateOf<String?>(null)
-
-    private val repository: ProductRepository
-
-    init {
-        val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8087/")
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val api = retrofit.create(ApiService::class.java)
-        repository = ProductRepository(api)
-        loadProducts()
-    }
-
-    fun loadProducts() {
+    fun fetchProducts() {
         viewModelScope.launch {
-            isLoading.value = true
             try {
-                val list = repository.fetchAll()
-                _products.clear()
-                _products.addAll(list)
-                errorMessage.value = null
+                val products = RetrofitInstance.apiService.getAll()
+                // Actualiza la UI con los productos
             } catch (e: Exception) {
-                errorMessage.value = e.localizedMessage
-            } finally {
-                isLoading.value = false
+                // Maneja el error
             }
         }
     }
 
-    fun addProduct(product: Product, onComplete: () -> Unit) {
+    fun createProduct(product: Product) {
         viewModelScope.launch {
-            isLoading.value = true
             try {
-                repository.add(product)
-                loadProducts()
-                onComplete()
+                val createdProduct = RetrofitInstance.apiService.create(product)
+                // Actualiza la UI con el producto creado
             } catch (e: Exception) {
-                errorMessage.value = e.localizedMessage
-            } finally {
-                isLoading.value = false
+                // Maneja el error
             }
         }
     }
